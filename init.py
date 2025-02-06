b85d0b959f1b2b542ba74cbda82c4d983bdea36b
Bluezly <141844841+Bluezly@users.noreply.github.com>
2025-02-06 04:36:49 +0300

    Add files via upload

---
+++ init.py
@@ -0,0 +1,53 @@
+import platform
+import tempfile
+import urllib.request
+import os
+import sys
+import subprocess
+
+def get_target():
+    targets = {
+      'Linux': {
+        'x86_64': 'x86_64-unknown-linux-musl',
+        'i686': 'i686-unknown-linux-musl',
+        'arm': 'armv7-unknown-linux-musleabihf',
+        'arm64': 'aarch64-unknown-linux-musl',
+      },
+      'Darwin': {
+        'x86_64': 'x86_64-apple-darwin',
+        'arm64': 'aarch64-apple-darwin',
+      },
+      'Windows': {
+        'x86_64': 'x86_64-pc-windows-msvc',
+        'i686': 'i686-pc-windows-msvc',
+      },
+    }
+
+    system = platform.system()
+    arch = platform.machine()
+
+    if system not in targets:
+        raise Exception(f'Unsupported platform: {system}')
+
+    if arch not in targets[system]:
+        raise Exception(f'Unsupported CPU architecture: {arch}')
+
+    return targets[system][arch]
+    
+def run():
+    target = get_target()
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp:
+        r = urllib.request.urlopen(f'https://artifacts.tunshell.com/client-{target}')
+        tmp.write(r.read())
+        tmp.close()
+        try:
+            os.chmod(tmp.name, 0o755)
+            subprocess.run([tmp.name] + sys.argv[1:])
+        finally:
+            try:
+                os.unlink(tmp.name)
+            except:
+                pass
+
+run()
